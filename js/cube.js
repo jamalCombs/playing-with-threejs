4ee5536ce1de7b9a8129cea4b8cac2c163c69312
jcombs01 <jmc1208@nyu.edu>
2017-06-24 13:18:15 -0400

    first commit

---
+++ js/cube.js
@@ -0,0 +1,14 @@
+class Cube {
+
+	constructor () {
+		this.type = 'Cube';
+		this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    	this.material = new THREE.MeshBasicMaterial( { color: 0xff00ff } );
+
+    	this.mesh = new THREE.Mesh( this.geometry, this.material );
+	}
+
+	getMesh() {
+		return this.mesh;
+	}
+}
