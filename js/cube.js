341509ee0db9b726fa397ccea440c70ff99ea25d
Jamal Combs <jamal.m.combs@gmail.com>
2018-03-21 00:11:13 -0400

    Update cube.js

--- js/cube.js
+++ js/cube.js
@@ -1,10 +1,8 @@
 class Cube {
-
 	constructor () {
 		this.type = 'Cube';
 		this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    	this.material = new THREE.MeshBasicMaterial( { color: 0xff00ff } );
-
+    	this.material = new THREE.MeshBasicMaterial({color: 0xff00ff});
     	this.mesh = new THREE.Mesh( this.geometry, this.material );
 	}
 
